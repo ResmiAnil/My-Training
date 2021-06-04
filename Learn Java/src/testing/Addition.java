6e3ae23713a82f9a680868d1bae594ca1ec9a5c4
resmi <resmi@DESKTOP-1AEN983>
2021-06-03 20:37:14 -0700

    adding two numbers

---
+++ Learn Java/src/testing/Addition.java
@@ -0,0 +1,28 @@
+package testing;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        NumberAddition nm=new NumberAddition();
+        nm.intAdd();
+	}
+
+}
+//class NumberAddition
+//{
+//// To add two numbers
+//	int num1;
+//	int num2;
+//	
+//	public void intAdd()
+//	{
+//		num1=3;
+//		num2=5;
+//		System.out.println("sum is "+(num1+num2));
+//	}
+//
+//
+//
+//
+//}
